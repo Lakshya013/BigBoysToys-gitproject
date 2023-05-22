201250d13575de329ce0750a5504f68f9c9473d7
Lakshya013 <112802205+Lakshya013@users.noreply.github.com>
2023-05-22 16:18:34 -0700

    newcommit

--- BigBoysToys/settings.py
+++ BigBoysToys/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'BigBoysToys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': 'BigBoysToys_db',
+        
     }
 }
 
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'BigBoysToys/static'),
+]
